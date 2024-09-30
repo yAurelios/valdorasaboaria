d0a3a6096b1e126418f34dac8c0583e90565b36a
João Kempfer <jp.aureliostk@gmail.com>
2024-09-30 11:46:02 -0300

    v0.9
    Eu atualizei os cards, mudei o scroll e fiz  a pg de contatos

--- src/js/header.js
+++ src/js/header.js
@@ -8,8 +8,8 @@ function loadHeader() {
             <ul>
                 <li><a href="../../src/html/inicio.html">Início</a></li>
                 <li><a href="../../src/html/produtos.html">Loja</a></li>
-                <li><a href="../../src/html/historia.html">Sobre mim</a></li>
-                <li><a href="">Contatos</a></li>
+                <li><a href="../../src/html/historia.html">História</a></li>
+                <li><a href="../../src/html/contatos.html">Contatos</a></li>
             </ul>
         </nav>
     </header>
--- teste/script.js
+++ teste/script.js
@@ -53,21 +53,17 @@ function loadProducts(container, title, description, products) {
     // Atualiza a visibilidade das setas
     updateArrowVisibility(leftArrow, rightArrow, products.length);
 
-
-
-    // TIRAR O COMENTÁRIO SE JULGAR MELHOR O SCROLL OU NÃO
-
     // Adiciona evento de rolagem
-    // productContainer.addEventListener('wheel', (event) => {
-    //     event.preventDefault(); // Previne o scroll padrão da página
-    //     if (event.deltaY > 0) {
-    //         moveSlide(1, innerProductContainer, products.length, leftArrow, rightArrow); // Para baixo
-    //     } else {
-    //         moveSlide(-1, innerProductContainer, products.length, leftArrow, rightArrow); // Para cima
-    //     }
-    // });
+    productContainer.addEventListener('wheel', (event) => {
+        event.preventDefault(); // Previne o scroll padrão da página
+        if (event.deltaY > 0) {
+            moveSlide(1, innerProductContainer, products.length, leftArrow, rightArrow); // Para baixo
+        } else {
+            moveSlide(-1, innerProductContainer, products.length, leftArrow, rightArrow); // Para cima
+        }
+    });
 
-    // adiciona evento para setas
+    // Adiciona evento para setas
     rightArrow.addEventListener('click', () => {
         moveSlide(1, innerProductContainer, products.length, leftArrow, rightArrow);
     });
@@ -78,20 +74,20 @@ function loadProducts(container, title, description, products) {
 }
 
 function moveSlide(direction, productListContainer, totalProducts, leftArrow, rightArrow) {
-    const visibleCount = 4; // num de produtos visíveis
+    const visibleCount = 4; // Número de produtos visíveis
     currentIndex += direction;
 
-    // limita o índice
+    // Limita o índice
     if (currentIndex < 0) {
         currentIndex = 0;
     } else if (currentIndex > totalProducts - visibleCount) {
         currentIndex = totalProducts - visibleCount;
     }
 
-    // atualiza a posição do container
+    // Atualiza a posição do container
     productListContainer.style.transform = `translateX(-${(currentIndex * 25)}%)`; // 25% para cada produto
 
-    // atualiza a visibilidade das setas
+    // Atualiza a visibilidade das setas
     updateArrowVisibility(leftArrow, rightArrow, totalProducts);
 }
 
@@ -101,27 +97,22 @@ function updateArrowVisibility(leftArrow, rightArrow, totalProducts) {
     rightArrow.style.display = currentIndex < totalProducts - visibleCount ? 'block' : 'none'; // Exibe se há elementos à direita
 }
 
-// chama a função com diferentes títulos, descrições e listas de produtos
+// Chama a função com diferentes títulos, descrições e listas de produtos
 document.addEventListener("DOMContentLoaded", () => {
     const productsContainer = document.querySelector('.products');
     loadProducts(productsContainer, "Promoção de Aniversário", "Descrição da promoção de aniversário!", [
-        { name: "Produto1", description: "Descrição do Produto 1", price: "R$ 100,00", img: "../../src/img/imagensSabonetes/img1.jpg" },
-        { name: "Produto2", description: "Descrição do Produto 2", price: "R$ 15,00", img: "../../src/img/imagensSabonetes/img2.jpg" },
-        { name: "Produto3", description: "Descrição do Produto 3", price: "R$ 20,00", img: "../../src/img/imagensSabonetes/img3.jpg" },
-        { name: "Produto4", description: "Descrição do Produto 4", price: "R$ 120,00", img: "../../src/img/imagensSabonetes/img4.jpg" },
-        { name: "Produto5", description: "Descrição do Produto 5", price: "R$ 25,00", img: "../../src/img/imagensSabonetes/img5.jpg" },
-        { name: "Produto6", description: "Descrição do Produto 6 ", price: "R$ 30,00", img: "../../src/img/imagensSabonetes/img6.jpg" },
-        { name: "Produto7", description: "Descrição do Produto 7", price: "R$ 30,00", img: "../../src/img/imagensSabonetes/img1.jpg" },
-        { name: "Produto8", description: "Descrição do Produto 8", price: "R$ 30,00", img: "../../src/img/imagensSabonetes/img4.jpg"}
+        { name: "Produto1", description: "Descrição do Produto 1", price: "R$ 10,00", img: "../src/img/imagensSabonetes/img1.jpg" },
+        { name: "Produto2", description: "Descrição do Produto 2", price: "R$ 15,00", img: "https://via.placeholder.com/150" },
+        { name: "Produto3", description: "Descrição do Produto 3", price: "R$ 20,00", img: "https://via.placeholder.com/150" },
+        { name: "Produto4", description: "Descrição do Produto 4", price: "R$ 120,00", img: "https://via.placeholder.com/150" },
+        { name: "Produto5", description: "Descrição do Produto 5", price: "R$ 25,00", img: "https://via.placeholder.com/150" },
+        { name: "Produto6", description: "Descrição do Produto 6", price: "R$ 30,00", img: "https://via.placeholder.com/150" },
+        { name: "Produto7", description: "Descrição do Produto 7", price: "R$ 30,00", img: "https://via.placeholder.com/150" },
+        { name: "Produto8", description: "Descrição do Produto 8", price: "R$ 30,00", img: "https://via.placeholder.com/150"}
     ]);
     loadProducts(productsContainer, "Promoção de Natal", "Descrição da promoção de aniversário!", [
-        { name: "Produto1", description: "Descrição do Produto 1", price: "R$ 10,00", img: "../../src/img/imagensSabonetes/img2.jpg" },
-        { name: "Produto2", description: "Descrição do Produto 2", price: "R$ 15,00", img: "../../src/img/imagensSabonetes/img5.jpg" },
-        { name: "Produto3", description: "Descrição do Produto 3", price: "R$ 20,00", img: "../../src/img/imagensSabonetes/img6.jpg" },
-    ]);
-    loadProducts(productsContainer, "Promoção de Perus de Natal", "Descrição da promoção de peruzinhos no seu cuzinhos!", [
-        { name: "Produto1", description: "Descrição do Produto 1", price: "R$ 10,00", img: "../../src/img/imagensSabonetes/img2.jpg" },
-        { name: "Produto2", description: "Descrição do Produto 2", price: "R$ 15,00", img: "../../src/img/imagensSabonetes/img5.jpg" },
-        { name: "Produto3", description: "Descrição do Produto 3", price: "R$ 20,00", img: "../../src/img/imagensSabonetes/img6.jpg" },
+        { name: "Produto1", description: "Descrição do Produto 1", price: "R$ 10,00", img: "https://via.placeholder.com/150" },
+        { name: "Produto2", description: "Descrição do Produto 2", price: "R$ 15,00", img: "https://via.placeholder.com/150" },
+        { name: "Produto3", description: "Descrição do Produto 3", price: "R$ 20,00", img: "https://via.placeholder.com/150" },
     ]);
 });
